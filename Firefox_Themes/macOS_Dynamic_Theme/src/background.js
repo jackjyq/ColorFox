c19e9d73af62740ee539219a0cf950fa853c8f5c
Jack Jiang <jackjyq@outlook.com>
2019-12-22 09:37:50 +1100

    update macOS dynamic theme

--- Firefox_Themes/macOS_Dynamic_Theme/src/background.js
+++ Firefox_Themes/macOS_Dynamic_Theme/src/background.js
@@ -37,44 +37,62 @@ function getLightTheme(color_name){
   };
 }
 
-function updateTheme() {
-  let date = new Date();
-  let hours = date.getHours();
-  let theme = ""
-  switch(hours % 8) {
-    case 0:
-      theme = getLightTheme('yellow')
-      break;
-    case 1:
-      theme = getLightTheme('blue')
-      break;
-    case 2:
-      theme = getLightTheme('pink')
-      break;
-    case 3:
-      theme = getLightTheme('green')
-      break;
-    case 4:
-      theme = getLightTheme('orange')
-      break;
-    case 5:
-      theme = getLightTheme('graphite')
-      break;
-    case 6:
-      theme = getLightTheme('red')
-      break;
-    case 7:
-      theme = getLightTheme('purple')
-      break;
-    default:
-      return;
-  }
-  browser.theme.update(theme);
+function getPrivateTheme(color_name){
+  return {
+    "colors": {
+      "toolbar": "rgb(250, 250, 250)",
+      "bookmark_text": "rgb(71, 71, 71)",
+      "frame": "rgb(37, 0, 62)",
+      "frame_inactive": "rgb(71, 34, 96)",
+      "tab_background_text": "white",
+      "toolbar_field": "rgb(250, 250, 250)",
+      "toolbar_field_text": "black",
+      "toolbar_field_highlight_text": "black",
+      "toolbar_field_border": "rgb(167,167,167)",
+      "popup": "rgb(250, 250, 250)",
+      "popup_text": "rgb(71, 71, 71)",
+      "popup_highlight_text": "white",
+      "sidebar_highlight_text": "white",
+      "tab_line": "rgb(" + macOS_colors[color_name] + ")",
+      "tab_loading": "rgb(" + macOS_colors[color_name] + ")",
+      "icons_attention": "rgb(" + macOS_colors[color_name] + ")",
+      "popup_highlight": "rgb(" + macOS_colors[color_name] + ")",
+      "sidebar_highlight": "rgb(" + macOS_colors[color_name] + ")",
+      "icons_attension": "rgb(" + macOS_colors[color_name] + ")",
+      "toolbar_field_border_focus": "rgb(" + macOS_colors[color_name] + ")",
+      "toolbar_field_highlight": "rgba(" + macOS_colors[color_name] +", 0.2)"
+    }
+  };
 }
 
-// On start up, check the time to see what theme to show.
-updateTheme();
+const color_sequence = ['blue', 'purple', 'pink', 'red',
+                        'orange', 'yellow', 'green', 'graphite']
 
-// Set up an alarm to check this regularly.
-browser.alarms.onAlarm.addListener(updateTheme);
-browser.alarms.create('updateTheme', {periodInMinutes: 1});
+function updateTheme(window) {
+  const date = new Date();
+  const hours = date.getHours();
+  // Check if the window is in private browsing
+  if (window.incognito) {
+    const theme = getPrivateTheme(color_sequence[hours % 8]);
+    browser.theme.update(window.id, theme);
+  }
+  // apply light theme when not in private browsing
+  else {
+    const theme = getLightTheme(color_sequence[hours % 8]);
+    browser.theme.update(window.id, theme);
+  }
+}
+
+function updateAllThemes() {
+  browser.windows.getAll().then(wins => wins.forEach(updateTheme));
+}
+
+// setup listeners
+// update all themes regularly
+browser.alarms.onAlarm.addListener(updateAllThemes);
+browser.alarms.create('updateAllThemes', {periodInMinutes: 1});
+// update theme when opening a new windows
+browser.windows.onCreated.addListener(updateAllThemes);
+
+// Initialization
+updateAllThemes()
